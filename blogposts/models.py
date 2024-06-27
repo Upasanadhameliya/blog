8b9d4319cd5d0eed305c35eeff75fff7fdc72387
Upasana Dhameliya (Ana) <upasana.dhameliya@gmail.com>
2024-06-27 18:11:38 -0400

    feat: Gallery Images model

--- blogposts/models.py
+++ blogposts/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from modelcluster.fields import ParentalKey
 
 # Create your models here.
-from wagtail.models import Page
+from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, InlinePanel
 
 from wagtail.search import index
 
@@ -13,6 +14,12 @@ class BlogPostsIndexPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel("intro")
     ]
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('first_published_at')
+        context['blogpages'] = blogpages
+        return context
 
 class BlogPostPage(Page):
     date = models.DateField("Post date")
@@ -27,6 +34,17 @@ class BlogPostPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel("date"),
         FieldPanel("intro"),
-        FieldPanel("body")
+        FieldPanel("body"),
+
+        InlinePanel('gallery_images', label="Gallery images")
     ]
 
+class BlogGalleryImages(Orderable):
+    blogpost = ParentalKey(BlogPostPage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey('wagtailimages.Image', on_delete=models.CASCADE, related_name='+')
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        FieldPanel('image'),
+        FieldPanel('caption')
+    ]
